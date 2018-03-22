a20fae56ee5196a9cb51aecb7fc4cbfe09afbbde
MinBarry <minna.barry@gmail.com>
2018-03-22 14:59:28 -0700

    Add scripts

---
+++ Scripts/ManagerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerScript : MonoBehaviour {
+
+    public static ManagerScript Instance { get; private set; }
+    public int Edges=4;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 mousepoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Debug.Log(mousepoint.x);
+
+        if (mousepoint.x <= ManagerScript.Instance.Edges && mousepoint.x >= -ManagerScript.Instance.Edges)
+            transform.position = new Vector3(mousepoint.x * speed, transform.position.y, transform.position.z);
+        else
+            Debug.Log("Shouldnt move");
+    }
+}
